3f9afe1835f8c29b38a36b09eb7152bb96388f82
Louis Krause <louis.kwame2@gmail.com>
2017-03-14 23:49:14 -0400

    Revert "add white space"
    This reverts commit 18600ceed4b631a7aea2ed52398179ca4c6e6ff2.

--- app/src/main/java/com/coreelements/ce/dungeon/Room_3buero.java
+++ app/src/main/java/com/coreelements/ce/dungeon/Room_3buero.java
@@ -74,8 +74,6 @@ public class Room_3buero extends AppCompatActivity implements View.OnKeyListener
         }
     }
 
-
-
     public void openSecretFloor(){
         if (sharedPref.getString("time", "null").contentEquals("01:36")){
             secretEditor.putBoolean("secret", true);
